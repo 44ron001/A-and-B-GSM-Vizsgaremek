5120f3fd14755c66bcc19457fc0b6828c91d555b
Bence2345 <bence1048@gmail.com>
2026-01-14 11:20:48 +0100

    Elso gomb

--- Asztali/ABGSM/Form1.cs
+++ Asztali/ABGSM/Form1.cs
@@ -16,6 +16,15 @@ namespace ABGSM
         {
             InitializeComponent();
             
+            this.Size = new Size(1280, 720);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Button button1 = new Button();
+            button1.Text = "Belepes";
+            button1.Size = new Size(200, 50);
+            button1.Location = new Point(300, 200);
+            button1.Visible = true;
+            this.Controls.Add(button1);
         }
     }
 }
