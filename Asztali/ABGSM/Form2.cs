49a3a128d1955ee8593a6984be9348f94dc9e399
Bence2345 <bence1048@gmail.com>
2026-02-16 11:42:59 +0100

    Apiba írás

--- Asztali/ABGSM/Form2.cs
+++ Asztali/ABGSM/Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,57 +17,40 @@ namespace ABGSM
         public Form2()
         {
             InitializeComponent();
+            this.Load += Form2_Load;
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            using (MySqlConnection conn = new MySqlConnection("server=localhost;uid=root;database=pcshop;port=3307;pwd=;"))
+            aaa();
+        }
+
+        public void aaa()
+        {
+            this.Text = "fasz+";
+            HttpClient client = new HttpClient();
+            string url = "http://localhost:3001/api/products/2";
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            this.Text = response.IsSuccessStatusCode.ToString();
+            if (response.IsSuccessStatusCode)
             {
-                conn.Open();
-
-                string sql = "SELECT * FROM products";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-
-                using (MySqlDataReader reader = cmd.ExecuteReader())
-                {
-                    listBox1.Items.Clear();
-
-                    while (reader.Read())
-                    {
-                        listBox1.Items.Add(reader["pID"].ToString() + "; " + reader["nev"].ToString());
-                    }
-                }
+                string json = response.Content.ReadAsStringAsync().Result;
+                //Clipboard.SetText(responseData);
+                listBox1.Items.Clear();
+                //listBox1.Items.Add(responseData);
+                //DESZERIALIZÁLÁS
+            }
+            else
+            {
+                listBox1.Items.Add("Error: " + response.StatusCode);
             }
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedItem == null)
-            {
-                MessageBox.Show("Select an item first!");
-                return;
-            }
-
-            string selected = listBox1.SelectedItem.ToString();
-
-            MessageBox.Show(selected);
-
-
-            int productId = int.Parse(selected.Split(';')[0].Trim());
-
-            using (MySqlConnection conn = new MySqlConnection(
-                "server=localhost;uid=root;database=pcshop;port=3307;pwd=;"))
-            {
-                conn.Open();
-
-                string sql = "DELETE FROM products WHERE pID = @pID";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                cmd.Parameters.AddWithValue("@pID", productId);
-
-                cmd.ExecuteNonQuery();
-            }
-
-            listBox1.Items.Remove(listBox1.SelectedItem);
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
